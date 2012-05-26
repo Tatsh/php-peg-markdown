b7d3d89e62c100c37467759879be5403989f362f
Tatsh <ddrtist@gmail.com>
2012-05-26 00:14:19 -0500

    Remove setting of CFLAGS and LDFLAGS

--- ltinit-fix-make.sh
+++ ltinit-fix-make.sh
@@ -5,5 +5,5 @@ aclocal
 libtoolize --force
 autoheader
 autoconf
-LDFLAGS="$LDFLAGS $(pkg-config glib-2.0 --libs)" CFLAGS="$CFLAGS $(pkg-config glib-2.0 --cflags)" ./configure --enable-markdown
+./configure --enable-markdown
 make
