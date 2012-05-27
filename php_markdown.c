ff3047927e94e96567a3a0c1e1376a1f5b96f420
Tatsh <ddrtist@gmail.com>
2012-05-27 10:10:50 -0500

    Add license information

--- php_markdown.c
+++ php_markdown.c
@@ -1,3 +1,21 @@
+/*
+ * +----------------------------------------------------------------------+
+ * | PHP Version 5                                                        |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 1997-2012 The PHP Group                                |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 3.01 of the PHP license,      |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.php.net/license/3_01.txt                                  |
+ * | If you did not receive a copy of the PHP license and are unable to   |
+ * | obtain it through the world-wide-web, please send a note to          |
+ * | license@php.net so we can mail you a copy immediately.               |
+ * +----------------------------------------------------------------------+
+ * | Author: Andrew Udvare <audvare@gmail.com>                            |
+ * +----------------------------------------------------------------------+
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -62,7 +80,11 @@ PHP_RSHUTDOWN_FUNCTION(markdown) {
 
 PHP_MINFO_FUNCTION(markdown) {
   php_info_print_table_start();
-  php_info_print_table_header(2, "markdown parsing", "enabled");
+  php_info_print_table_row(2, "Markdown parsing", "enabled");
+  php_info_print_table_end();
+
+  php_info_print_table_start();
+  php_info_print_table_header(1, "markdown makes use of \"peg-markdown\" which is distributed under the GNU Lesser General Public License version 2 and MIT licenses.");
   php_info_print_table_end();
 }
 
