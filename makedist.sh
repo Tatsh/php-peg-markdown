e60126a63c0684d1dd17004d58d3bb17ca8fb5ac
Tatsh <ddrtist@gmail.com>
2012-07-01 14:10:30 -0700

    Forcibly remove

--- makedist.sh
+++ makedist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -R markdown-1.0 markdown-1.0.tar.bz2
+rm -fR markdown-1.0 markdown-1.0.tar.bz2
 mkdir markdown-1.0
 for i in config.m4 config.w32 ltinit-fix-make.sh php_markdown.c php_markdown.h;
 	do cp "$i" markdown-1.0/
