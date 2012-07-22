15738404661c9553bd744fb31d5309cd1deb8f4c
Tatsh <audvare@gmail.com>
2012-07-22 16:38:06 -0700

    Use version variable

--- makedist.sh
+++ makedist.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-rm -fR markdown-1.0 markdown-1.0.tar.bz2
-mkdir markdown-1.0
+VERSION="1.01"
+
+rm -fR markdown-$VERSION markdown-$VERSION.tar.bz2
+mkdir markdown-$VERSION
 for i in config.m4 config.w32 ltinit-fix-make.sh php_markdown.c php_markdown.h;
-	do cp "$i" markdown-1.0/
+	do cp "$i" markdown-$VERSION/
 done
 
-tar cjvf markdown-1.0.tar.bz2 markdown-1.0
-rm -R markdown-1.0
+tar cjvf markdown-$VERSION.tar.bz2 markdown-$VERSION
+rm -R markdown-$VERSION
